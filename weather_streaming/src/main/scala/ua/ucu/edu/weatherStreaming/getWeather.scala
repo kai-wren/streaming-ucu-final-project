de8b35f79c0632294bde436385ea53ff2715537c
svlsml <svsmailova@gmail.com>
2020-03-15 17:36:47 +0200

    minor fixings

--- weather_streaming/src/main/scala/ua/ucu/edu/weatherStreaming/getWeather.scala
+++ weather_streaming/src/main/scala/ua/ucu/edu/weatherStreaming/getWeather.scala
@@ -13,19 +13,14 @@ object WeatherAPI {
     val url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=04c7561284bb760addde892b62840442"
     val result = scala.io.Source.fromURL(url).mkString
       .replace("main", "mainWeather")
-    //    println(result)
     result
   }
 
   def parseJson(json: String): String ={
     implicit val formats = DefaultFormats
-    //    val weather: String = new WeatherAPI().getWeatherApi("Lviv").toString()
     val parsedJson = parse(json)    //get json from openweathermap API
     val parsed = parsedJson.extract[Weather]    // get Weather class with air and wind data
-//    val airItems = List(parsed.mainWeather.temp, parsed.mainWeather.pressure, parsed.mainWeather.humidity)
     val jsonString = write(parsed)        // convert Weather class to json string
-  //  airItems.mkString(",")
-
     jsonString
     }
 
@@ -44,7 +39,6 @@ object WeatherAPI {
     implicit val formats = DefaultFormats
     case class WindComponents(name: String, wind: windWeather)
     case class WindWeatherFlatten(name: String, windSpeed: String, windDeg: String)
-//    val jsonWeatherString = parseJson(json)
     val parsedJson = parse(jsonWeatherString)
     val parsed = parsedJson.extract[WindComponents]
     val flattenParsed = WindWeatherFlatten(parsed.name, parsed.wind.speed, parsed.wind.deg)
--- weather_streaming/src/main/scala/ua/ucu/edu/weatherStreaming/producerWeather.scala
+++ weather_streaming/src/main/scala/ua/ucu/edu/weatherStreaming/producerWeather.scala
@@ -4,47 +4,6 @@ import akka.actor.typed.{Behavior, PostStop, Signal}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import ua.ucu.edu.weatherStreaming.producerWeather.{Response, Start, Stop}
 
-
-//object producerWeather2 {
-//  def apply(): Behavior[Command] = Behaviors.setup(context => new producerWeather2(context))
-//
-//  case class Start() extends Command
-//  case class Stop() extends Command
-//  case class Response(response: cityWeather.RespondTemp) extends Command
-//}
-
-//class producerWeather2(context: ActorContext[Command]) extends AbstractBehavior [Command]{
-//  val respondTemp = context.messageAdapter(Response.apply)
-//  val airDataTopic = "aqi-weather-streaming"
-//
-//  override def onMessage(msg: Command): Behavior[Command] = {
-//    msg match{
-//      case Start() =>
-//        val citySignal = context.spawn(cityWeather("Lviv"), "Lviv-Weather")
-//        citySignal !  cityWeather.ReadTemp("Lviv", respondTemp)
-//        this
-//      case Stop() => Behavior.stopped
-//      case Response(response) => onRespondTemperature(response)
-//      this
-//    }
-//
-//    def onRespondTemperature(response: cityWeather.RespondTemp):Behavior[Command] = {
-//      val reading: String = response.value match {
-//        case value => value.toString()
-//        case _ => "None"
-//      }
-//      val temperatureValue = WeatherAPI.parseJson(reading)
-//      weatherKafkaProducer.produceRecord(airDataTopic, response.city, temperatureValue.toString())
-//      this
-//
-//    }
-//
-//  }
-//
-//
-//}
-
-
 object producerWeather {
   def apply(): Behavior[Command] =
     Behaviors.setup(context => new producerWeather(context))
@@ -57,7 +16,6 @@ object producerWeather {
 class producerWeather(context: ActorContext[Command]) extends AbstractBehavior[Command] {
 
   private val respondTemperatureAdapter = context.messageAdapter(Response.apply)
-//  private val token = "f84d40a229949596ed1d0748a0cb6e4314b0a74c"
   private val airDataTopic = "aqi-weather-streaming"
   private val windDataTopic = "aqi-weather-wind-streaming"
   private val cities = List("lviv", "kyiv", "odesa", "zaporizhzhya", "ivano-frankivsk", "dnipro", "rivne", "ternopil",
@@ -66,8 +24,6 @@ class producerWeather(context: ActorContext[Command]) extends AbstractBehavior[C
   override def onMessage(msg: Command): Behavior[Command] =
     msg match {
       case Start() =>
-//        val device = context.spawn(cityWeather("Lviv"), "LvivWeather")
-//        device ! cityWeather.ReadTemp("Lviv", respondTemperatureAdapter)
         for (city <-  cities){
           val citySignal = context.spawn(cityWeather(city), s"${city}-weather")
           citySignal ! cityWeather.ReadTemp(city, respondTemperatureAdapter)
